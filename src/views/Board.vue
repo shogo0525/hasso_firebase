2fdc673a25f406b611426e85e0caaee82d7013d7
shogo_inamoto <shogo.ca2011@gmail.com>
2019-03-24 15:23:20 +0900

    [clean]refactor Board.vue

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="board">
-    <h1 v-show="!edit_board_name" @click="edit_board_name = true">ボード名: {{ board_name }}</h1>
+  <div v-if="board" class="board">
+    <h1 v-show="!edit_board_name" @click="edit_board_name = true">ボード名: {{ board.name }}</h1>
     <div v-show="edit_board_name">
       <v-text-field
         class="input_board_name"
-        v-model="board_name"
+        v-model="board.name"
         label="ボード名"
         outline
       ></v-text-field>
@@ -47,92 +47,99 @@
 </template>
 
 <script>
+import firebase from 'firebase'
 import { db } from '@/firebase/firestore'
 
 export default {
-  name: 'board_show',
+  name: 'board-show',
   data() {
     return {
       boardRef: "",
       postsRef: "",
       edit_board_name: false,
-      board_id: "",
-      board_name: "",
+      board: null,
       post_text: "",
       posts: []
     }
   },
   created() {
-    this.post_id = this.$route.params.id
-    this.boardRef = db.collection('boards').doc(this.post_id)
+    this.boardRef = db.collection('boards').doc(this.$route.params.id)
     this.postsRef = this.boardRef.collection('posts')
 
-    this.boardRef.onSnapshot((doc) => {
-      if (doc.exists) {
-        this.board_name = doc.data().name
-      } else {
-        console.log("No such document!") // eslint-disable-line
-      }
-    })
-
-    this.postsRef.orderBy("created_at", "asc").onSnapshot((snapshot) => {
-      snapshot.docChanges().forEach(change => {
-        if (change.type == "added") {
-          let like = 0
-          if (change.doc.data().like) like = change.doc.data().like
-          const post = {
-            id: change.doc.id,
-            text: change.doc.data().text,
-            like: like
-          }
-          this.posts.unshift(post)
-        } else if (change.type == "modified") {
-          this.posts.filter(post => {
-            if(post.id == change.doc.id) {
-              post.like = change.doc.data().like
-            }
-          })
-        } else if (change.type == "removed") {
-          this.posts = this.posts.filter(post => post.id !== change.doc.id)
-        }
-      })
-    })
+    this.fetchBoard()
+    this.fetchPosts()
   },
   methods: {
+    async fetchBoard() {
+      const doc = await this.boardRef.get()
+      if (!doc.exists) {
+        this.$router.push({ name: 'home' })
+        return
+      }
+      this.board = doc.data()
+    },
+    async fetchPosts() {
+      this.postsRef.orderBy("created_at", "asc").onSnapshot((snapshot) => {
+        snapshot.docChanges().forEach(change => {
+          if (change.type === "added") {
+            const like = change.doc.data().like || 0
+            const post = {
+              id: change.doc.id,
+              text: change.doc.data().text,
+              like
+            }
+            this.posts.unshift(post)
+          } else if (change.type === "modified") {
+            const post = this.posts.find(post => post.id === change.doc.id)
+            post.like = change.doc.data().like
+          } else if (change.type === "removed") {
+            this.posts = this.posts.filter(post => post.id !== change.doc.id)
+          }
+        })
+      })
+    },
     updateBoardname() {
-      if (this.board_name.length == 0) {
+      if (this.board.name.length == 0) {
         alert("ボート名を入力してください")
         return
       }
       this.boardRef.set({
-        name: this.board_name,
-      })
+        name: this.board.name,
+      }, { merge: true })
       this.edit_board_name = false
     },
-    createNewPost() {
+    async createNewPost() {
       if (this.post_text.length == 0) {
         alert("ポストを入力してください")
         return
       }
-      const post_text = this.post_text
       
-      this.postsRef.add({
-        text: post_text,
-        created_at: new Date()
-      })
-      .then(() => this.post_text = "")
-      .catch((error)  => console.error('Error adding document: ', error)) // eslint-disable-line
+      try {
+        await this.postsRef.add({
+          text: this.post_text,
+          created_at: firebase.firestore.Timestamp.now()
+        })
+        this.post_text = ""
+      } catch (e) {
+        console.error('Error adding document: ', e) // eslint-disable-line
+      }
     },
     deletePost(post_id) {
-      this.postsRef.doc(post_id).delete()
-      .catch((error) => console.error("Error removing document: ", error)) // eslint-disable-line
+      try {
+        this.postsRef.doc(post_id).delete()
+      } catch (e) {
+        console.error('Error removing document: ', e) // eslint-disable-line
+      }
     },
     likePost(post_id) {
-      const post = this.posts.filter(post => post.id == post_id)
-      this.postsRef.doc(post_id).update({
-        like: post[0].like + 1
-      })
-      .catch((error) =>console.error('Error adding document: ', error)) // eslint-disable-line
+      try {
+        const post = this.posts.filter(post => post.id === post_id)
+        this.postsRef.doc(post_id).set({
+          like: post[0].like + 1
+        }, { merge: true })
+      } catch (e) {
+        console.error('Error liking document: ', e) // eslint-disable-line
+      }
     },
     getTwitterUrl(post_text) {
       return 'https://twitter.com/intent/tweet?text=' + post_text + '&hashtags=Webナイト宮崎'
